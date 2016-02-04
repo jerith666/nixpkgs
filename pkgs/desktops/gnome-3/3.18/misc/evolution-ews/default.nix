cdc4b5f7cc1ef3f9a318ff0a5afedae6336f74c3
Matt McHenry <github@matt.mchenryfamily.org>
2016-02-04 16:23:30 -0500

    try with gtk3, still not working

--- pkgs/desktops/gnome-3/3.18/misc/evolution-ews/default.nix
+++ pkgs/desktops/gnome-3/3.18/misc/evolution-ews/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchgit, gnome3, glib, intltool, pkgconfig, gtk_doc, libtool, gtk, libsoup, sqlite, webkitgtk } :
+{ stdenv, fetchgit, gnome3, glib, intltool, pkgconfig, gtk_doc, libtool, gtk3, libsoup, sqlite, webkitgtk } :
 
 let
   version = "3.18.4";
@@ -15,7 +15,7 @@ stdenv.mkDerivation {
   };
 
   buildInputs = [ gnome3.gnome_common gnome3.evolution_data_server gnome3.evolution
-                  glib intltool pkgconfig gtk_doc libtool gtk libsoup sqlite webkitgtk ];
+                  glib intltool pkgconfig gtk_doc libtool gtk3 libsoup sqlite webkitgtk ];
 
   buildPhase = ''
     ./autogen.sh;
