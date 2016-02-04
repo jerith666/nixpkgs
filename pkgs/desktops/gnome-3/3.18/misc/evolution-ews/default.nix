496499a37d3dedd1871d80e2acaee15f476d3019
Matt McHenry <github@matt.mchenryfamily.org>
2016-02-04 16:42:05 -0500

    need webkit gtk 2.4.x ... sigh

--- pkgs/desktops/gnome-3/3.18/misc/evolution-ews/default.nix
+++ pkgs/desktops/gnome-3/3.18/misc/evolution-ews/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchgit, gnome3, glib, intltool, pkgconfig, gtk_doc, libtool, gtk3, libsoup, sqlite, webkitgtk } :
+{ stdenv, fetchgit, gnome3, glib, intltool, pkgconfig, gtk_doc, libtool, gtk3, libsoup, sqlite, webkitgtk24x } :
 
 let
   version = "3.18.4";
@@ -15,7 +15,7 @@ stdenv.mkDerivation {
   };
 
   buildInputs = [ gnome3.gnome_common gnome3.evolution_data_server gnome3.evolution
-                  glib intltool pkgconfig gtk_doc libtool gtk3 libsoup sqlite webkitgtk ];
+                  glib intltool pkgconfig gtk_doc libtool gtk3 libsoup sqlite webkitgtk24x ];
 
   buildPhase = ''
     ./autogen.sh;
