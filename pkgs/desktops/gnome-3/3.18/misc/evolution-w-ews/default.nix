622523a33b1ced059e858b5b6b8f42cb0c6d859d
Matt McHenry <github@matt.mchenryfamily.org>
2016-03-24 14:26:12 -0400

    use fhs for evolution-w-ews

--- pkgs/desktops/gnome-3/3.18/misc/evolution-w-ews/default.nix
+++ pkgs/desktops/gnome-3/3.18/misc/evolution-w-ews/default.nix
@@ -1,2 +1,11 @@
-{ stdenv }:
+{ lib, pkgs, buildFHSUserEnv }:
 
+buildFHSUserEnv {
+  name = "evolution-w-ews";
+
+  targetPkgs = pkgs: with pkgs; [ evolution-data-server,
+                                  evolution,
+                                  evoltion-ews ];
+
+  runScript = "evolution";
+}
