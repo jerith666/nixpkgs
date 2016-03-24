28983cacd2532c11e0140c28a7e9577bd226736f
Matt McHenry <github@matt.mchenryfamily.org>
2016-03-24 15:43:50 -0400

    dump in a bunch more targetPkgs, from evolution's build inputs ... still not working though

--- pkgs/desktops/gnome-3/3.18/misc/evolution-w-ews/default.nix
+++ pkgs/desktops/gnome-3/3.18/misc/evolution-w-ews/default.nix
@@ -3,9 +3,20 @@
 buildFHSUserEnv {
   name = "evolution-w-ews";
 
-  targetPkgs = pkgs: with pkgs; [ evolution-data-server,
-                                  evolution,
-                                  evoltion-ews ];
+  targetPkgs = pkgs: with pkgs;
+                     ( [ pkgconfig gtk3 glib intltool itstool libxml2 libtool
+                  gdk_pixbuf gnome3.defaultIconTheme librsvg db icu
+                  gnome3.evolution_data_server libsecret libical #gcr
+                  webkitgtk shared_mime_info gnome3.gnome_desktop gtkspell3
+                  libcanberra_gtk3 bogofilter gnome3.libgdata sqlite
+                  gst_all_1.gstreamer gst_all_1.gst-plugins-base p11_kit
+                  nss nspr libnotify procps highlight gnome3.libgweather
+                  gnome3.gsettings_desktop_schemas makeWrapper ] )
+                  ++ ( [ dbus.tools ] )
+                  ++ (with gnome3; [ evolution_data_server
+                                     evolution
+                                     evolution-ews ]);
 
-  runScript = "evolution";
+  #runScript = "evolution";
+  runScript = "bash";
 }
