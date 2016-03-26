cf370998afb45ad5b7f3928bc7ec07800b23f4a1
Matt McHenry <github@matt.mchenryfamily.org>
2016-03-25 22:55:05 -0400

    try to get all inputs recursively

--- pkgs/desktops/gnome-3/3.18/misc/evolution-w-ews/default.nix
+++ pkgs/desktops/gnome-3/3.18/misc/evolution-w-ews/default.nix
@@ -1,16 +1,18 @@
-{ lib, pkgs, buildFHSUserEnv }:
+{ lib, buildFHSUserEnv }:
 
 let
 
-  allInputs = p: p.buildInputs ++
-                 builtins.concatLists (map allInputs p.buildInputs)
+  allInputs = p: if (builtins.isAttrs p && builtins.hasAttr "nativeBuildInputs" p)
+                    then (p.nativeBuildInputs ++
+                          (builtins.concatLists (map allInputs p.nativeBuildInputs)))
+                    else [];
 
 in
 
 buildFHSUserEnv {
   name = "evolution-w-ews";
 
-  targetPkgs = allInputs gnome3.evolution-ews;
+  targetPkgs = pkgs: [pkgs.dbus.tools] ++ (allInputs pkgs.gnome3.evolution-ews);
 
   #runScript = "evolution";
   runScript = "bash";
