458daae74ff31fa2adb0bc1c22ae0370daec4fc7
Matt McHenry <github@matt.mchenryfamily.org>
2016-04-10 21:54:40 -0400

    fix 1.0.24 urls

--- pkgs/applications/graphics/sane/backends/default.nix
+++ pkgs/applications/graphics/sane/backends/default.nix
@@ -5,8 +5,8 @@ callPackage ./generic.nix (args // rec {
   src = fetchurl {
     sha256 = "0ba68m6bzni54axjk15i51rya7hfsdliwvqyan5msl7iaid0iir7";
     urls = [
-      "http://pkgs.fedoraproject.org/repo/pkgs/sane-backends/sane-backends-1.0.24.tar.gz/1ca68e536cd7c1852322822f5f6ac3a4/${name}.tar.gz"
-      "https://alioth.debian.org/frs/download.php/file/3958/${name}.tar.gz"
+      "http://pkgs.fedoraproject.org/repo/pkgs/sane-backends/sane-backends-${version}.tar.gz/1ca68e536cd7c1852322822f5f6ac3a4/sane-backends-${version}.tar.gz"
+      "https://alioth.debian.org/frs/download.php/file/3958/sane-backends-${version}.tar.gz"
     ];
     curlOpts = "--insecure";
   };
