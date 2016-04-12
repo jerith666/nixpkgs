6f0cddd3ce8c6960fb2882cf0ef32faab54ba5e5
Matt McHenry <github@matt.mchenryfamily.org>
2016-04-11 22:25:45 -0400

    Revert "sane-backends: revert to 1.0.24"
    This reverts commit 1573b30acc4b40e41a9915dc0e2856f5d28cf9f1.

--- pkgs/applications/graphics/sane/backends/default.nix
+++ pkgs/applications/graphics/sane/backends/default.nix
@@ -1,12 +1,12 @@
 { callPackage, fetchurl, ... } @ args:
 
 callPackage ./generic.nix (args // rec {
-  version = "1.0.24";
+  version = "1.0.25";
   src = fetchurl {
-    sha256 = "0ba68m6bzni54axjk15i51rya7hfsdliwvqyan5msl7iaid0iir7";
+    sha256 = "0b3fvhrxl4l82bf3v0j47ypjv6a0k5lqbgknrq1agpmjca6vmmx4";
     urls = [
-      "http://pkgs.fedoraproject.org/repo/pkgs/sane-backends/sane-backends-1.0.24.tar.gz/1ca68e536cd7c1852322822f5f6ac3a4/${name}.tar.gz"
-      "https://alioth.debian.org/frs/download.php/file/3958/${name}.tar.gz"
+      "http://pkgs.fedoraproject.org/repo/pkgs/sane-backends/sane-backends-${version}.tar.gz/f9ed5405b3c12f07c6ca51ee60225fe7/sane-backends-${version}.tar.gz"
+      "https://alioth.debian.org/frs/download.php/file/4146/sane-backends-${version}.tar.gz"
     ];
     curlOpts = "--insecure";
   };
