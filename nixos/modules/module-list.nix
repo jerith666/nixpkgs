019b003b2c70ed71faa373a7092cd486a5de147d
Matt McHenry <github@matt.mchenryfamily.org>
2016-04-20 22:10:52 -0400

    nixos/tinydns: add module

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -374,6 +374,7 @@
   ./services/networking/tcpcrypt.nix
   ./services/networking/teamspeak3.nix
   ./services/networking/tinc.nix
+  ./services/networking/tinydns.nix
   ./services/networking/tftpd.nix
   ./services/networking/tlsdated.nix
   ./services/networking/tox-bootstrapd.nix
