d280963663cd7168d6d0c7a39b1d3880400d909c
Matt McHenry <github@matt.mchenryfamily.org>
2017-06-10 22:28:21 -0400

    Revert "try adding classifier, looks like suffix already handles it though"
    This reverts commit 93fbc0add96457b511804ab6b58e20ee3dcaa647.

--- pkgs/development/java-modules/m2install.nix
+++ pkgs/development/java-modules/m2install.nix
@@ -1,9 +1,9 @@
-{ stdenv, fetchurl, lib }:
-{ version, artifactId, groupId, sha512, type ? "jar", suffix ? "", classifier ? null }:
+{ stdenv, fetchurl }:
+{ version, artifactId, groupId, sha512, type ? "jar", suffix ? "" }:
 
 let
   name = "${artifactId}-${version}";
-  m2Path = "${builtins.replaceStrings ["."] ["/"] groupId}/${artifactId}/${version}" + (lib.optionalString (classifier != null) "-${classifier}");
+  m2Path = "${builtins.replaceStrings ["."] ["/"] groupId}/${artifactId}/${version}";
   m2File = "${name}${suffix}.${type}";
   src = fetchurl rec {
       inherit sha512;
