e37c8415d8939310bc79d17e36eee55266304b97
Matt McHenry <github@matt.mchenryfamily.org>
2017-10-17 21:38:21 -0400

    remove unused tinydns and dnscache ids

--- nixos/modules/misc/ids.nix
+++ nixos/modules/misc/ids.nix
@@ -296,8 +296,6 @@
       clickhouse = 278;
       rslsync = 279;
       minio = 280;
-      tinydns = 281;
-      dnscache = 282;
 
       # When adding a uid, make sure it doesn't match an existing gid. And don't use uids above 399!
 
@@ -563,8 +561,6 @@
       clickhouse = 278;
       rslsync = 279;
       minio = 280;
-      tinydns = 281;
-      dnscache = 282;
 
       # When adding a gid, make sure it doesn't match an existing
       # uid. Users and groups with the same name should have equal
