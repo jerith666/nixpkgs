edae4cde922505c7e54d84e1d9f0a0f93549b7e7
Matt McHenry <github@matt.mchenryfamily.org>
2019-09-02 10:50:06 -0400

    ensure system-result etc are not gc'd

--- rebuild-update.sh
+++ rebuild-update.sh
@@ -8,10 +8,10 @@ d=$1
 wt=$2
 
 nixos-rebuild build -I nixpkgs=$wt;
-
-mv -iv result system-result;
+nix-store --realise --add-root system-result --indirect result;
 
 nix-build -I nixpkgs=$wt -A pkgs.client-ip-echo;
+nix-store --realise --add-root client-ip-echo-result --indirect result;
 
 nix-shell -I nixpkgs=$wt ~/git/elbum/shell.nix --run true;
 
