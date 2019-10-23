e2f91a8cc8979c92fa5fd81b10bfa67ef64d736f
Matt McHenry <github@matt.mchenryfamily.org>
2019-10-22 21:44:06 -0400

    include our lib dir in our rpath

--- pkgs/os-specific/linux/ati-drivers/builder.sh
+++ pkgs/os-specific/linux/ati-drivers/builder.sh
@@ -231,7 +231,7 @@ fi
     libaticaldd.so \
     xorg/modules/linux/libfglrxdrm.so
   do
-    patchelf --set-rpath $glibcDir/lib/:$libStdCxx/lib/ $out/lib/$pelib2
+    patchelf --set-rpath $glibcDir/lib/:$libStdCxx/lib/:$out/lib/ $out/lib/$pelib2
   done
 }
 
