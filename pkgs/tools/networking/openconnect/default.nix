1cd09884c3ef42279fea6d67eebaa718c58e8dc3
Matt McHenry <github@matt.mchenryfamily.org>
2020-12-09 20:59:10 -0500

    openconnect: set OS variable to Darwin or Linux appropriately
    caught by @SuperSandro2000

--- pkgs/tools/networking/openconnect/default.nix
+++ pkgs/tools/networking/openconnect/default.nix
@@ -7,29 +7,32 @@
 
 assert (openssl != null) == (gnutls == null);
 
-let vpnc = stdenv.mkDerivation {
-  name = "vpnc-scripts-c0122e891f7";
-  src = fetchgit {
-    url = "git://git.infradead.org/users/dwmw2/vpnc-scripts.git";
-    rev = "c0122e891f7e033f35f047dad963702199d5cb9e";
-    sha256 = "11b1ls012mb704jphqxjmqrfbbhkdjb64j2q4k8wb5jmja8jnd14";
+let
+  os = if stdenv.isDarwin then "Darwin" else "Linux";
+
+  vpnc = stdenv.mkDerivation {
+    name = "vpnc-scripts-c0122e891f7";
+    src = fetchgit {
+      url = "git://git.infradead.org/users/dwmw2/vpnc-scripts.git";
+      rev = "c0122e891f7e033f35f047dad963702199d5cb9e";
+      sha256 = "11b1ls012mb704jphqxjmqrfbbhkdjb64j2q4k8wb5jmja8jnd14";
+    };
+
+    buildInputs = [ makeWrapper ];
+
+    installPhase = ''
+      mkdir -p $out/bin;
+      cp vpnc-script $out/bin;
+
+      substituteInPlace $out/bin/vpnc-script \
+        --replace "which" "type -P" \
+        --replace "/sbin/resolvconf" "${openresolv}/bin/resolvconf";
+
+      wrapProgram $out/bin/vpnc-script \
+        --set OS ${os} \
+        --prefix PATH : "${stdenv.lib.makeBinPath [ nettools gawk openresolv coreutils gnugrep ]}";
+    '';
   };
-
-  buildInputs = [ makeWrapper ];
-
-  installPhase = ''
-    mkdir -p $out/bin;
-    cp vpnc-script $out/bin;
-
-    substituteInPlace $out/bin/vpnc-script \
-      --replace "which" "type -P" \
-      --replace "/sbin/resolvconf" "${openresolv}/bin/resolvconf";
-
-    wrapProgram $out/bin/vpnc-script \
-      --set OS "Linux" \
-      --prefix PATH : "${stdenv.lib.makeBinPath [ nettools gawk openresolv coreutils gnugrep ]}";
-  '';
-};
 
 in stdenv.mkDerivation rec {
   pname = "openconnect";
