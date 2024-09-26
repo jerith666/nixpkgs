f32a144cce8318a05d7ced70997fab448d4844de
Matt McHenry <github@matt.mchenryfamily.org>
2024-09-26 18:24:20 -0400

    Revert "globalprotect-openconnect: remove deprecated 1.x package"
    This reverts commit b08d6a664f548c86446439a1e24e484b0ee140ea.
    
    Conflicts:
    	nixos/doc/manual/release-notes/rl-2411.section.md

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13544,6 +13544,8 @@ with pkgs;
 
   inherit (openconnectPackages) openconnect openconnect_openssl;
 
+  globalprotect-openconnect = libsForQt5.callPackage ../tools/networking/globalprotect-openconnect { };
+
   ding-libs = callPackage ../tools/misc/ding-libs { };
 
   sssd = callPackage ../os-specific/linux/sssd {
