b7d1d6817e124550a47bc6e8be2b741c1ba3d19e
Matt McHenry <github@matt.mchenryfamily.org>
2024-10-27 17:11:48 -0400

    k3b: use kdePackages.k3b
    as suggested in https://github.com/NixOS/nixpkgs/issues/348919

--- nixos/modules/programs/k3b.nix
+++ nixos/modules/programs/k3b.nix
@@ -23,7 +23,7 @@
   config = lib.mkIf config.programs.k3b.enable {
 
     environment.systemPackages = with pkgs; [
-      k3b
+      kdePackages.k3b
       dvdplusrwtools
       cdrdao
       cdrtools
