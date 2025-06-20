2ea82e8a6fef03aaa638077715818efc5f839772
Matt McHenry <github@matt.mchenryfamily.org>
2025-06-20 19:26:27 -0400

    Revert "around: update to use pname and version rather than name"
    This reverts commit 541f7399b9efe81bb633e90465556d3017c23d3d.

--- pkgs/applications/networking/instant-messengers/around/default.nix
+++ pkgs/applications/networking/instant-messengers/around/default.nix
@@ -4,7 +4,7 @@
 } :
 
 let
-  pname = "around";
+  name = "around";
   #when a required update is published, the launcher will fail
   #and output a line like:
   #desktopapp:updater Updater state changed:  {"type":"available","version":"0.54.8","downloadProgress":0,"isRequired":true} +118ms
@@ -15,18 +15,18 @@ let
     hash = "sha256-+h621GQx6H7wy+dEWi3RRPXOr7NUvXf9LfP/RXaPdhI=";
   };
 
-  appimageContents = appimageTools.extract { inherit pname src version; };
+  appimageContents = appimageTools.extract { inherit name src; };
 
 in
 
 appimageTools.wrapType2 {
-  inherit pname version src;
+  inherit name src;
 
   extraInstallCommands = ''
-    install -m 444 -D ${appimageContents}/${pname}.desktop \
+    install -m 444 -D ${appimageContents}/${name}.desktop \
       -t $out/share/applications
-    substituteInPlace $out/share/applications/${pname}.desktop \
-      --replace 'Exec=AppRun' "Exec=$out/bin/${pname}"
+    substituteInPlace $out/share/applications/${name}.desktop \
+      --replace 'Exec=AppRun' "Exec=$out/bin/${name}"
     cp -vr ${appimageContents}/usr/share/icons $out/share
   '';
 
