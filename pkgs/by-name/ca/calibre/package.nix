281173d7e69ad5958dac457d08cbdc0bf544ec3b
Matt McHenry <github@matt.mchenryfamily.org>
2026-02-26 21:08:22 -0500

    Revert "calibre: 8.16.2 -> 9.1.0"
    This reverts commit 96a27368d190d131bfffdcd5d94fe70738333431.

--- pkgs/by-name/ca/calibre/package.nix
+++ pkgs/by-name/ca/calibre/package.nix
@@ -25,30 +25,24 @@
   pkg-config,
   podofo_0_10,
   poppler-utils,
-  python314Packages,
+  python3Packages,
   qt6,
   speechd-minimal,
   sqlite,
   xdg-utils,
   wrapGAppsHook3,
   popplerSupport ? true,
-  speechSupport ? false, # piper-tts doesn't build with python 3.14
+  speechSupport ? true,
   unrarSupport ? false,
 }:
 
-let
-  # added during 8.x -> 9.x update; remove once nixpkgs default
-  # python version catches up
-  python3Packages = python314Packages;
-in
-
 stdenv.mkDerivation (finalAttrs: {
   pname = "calibre";
-  version = "9.1.0";
+  version = "8.16.2";
 
   src = fetchurl {
     url = "https://download.calibre-ebook.com/${finalAttrs.version}/calibre-${finalAttrs.version}.tar.xz";
-    hash = "sha256-D5fsl7UEnnMu0f5eRkwBOBSdzhyyPR2l9jKOnUoqoPM=";
+    hash = "sha256-AYfQQ1T1PMB0EUHaAml37jCnfvoMN7GDm94FiCIsHGw=";
   };
 
   patches =
