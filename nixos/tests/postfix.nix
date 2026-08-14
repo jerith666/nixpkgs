9b553c5ca9b0c991cc89c2b4f5b25c551e7f105b
Matt McHenry <github@matt.mchenryfamily.org>
2026-08-13 21:13:46 -0400

    nixos/tests/postfix.nix: remove duplicate auth scripts
    these must have crept in during a merge

--- nixos/tests/postfix.nix
+++ nixos/tests/postfix.nix
@@ -127,44 +127,6 @@ import ./make-test-python.nix {
                 smtp.login("alice", "foobar")
                 smtp.quit()
           '';
-
-          auth = pkgs.writeScriptBin "auth" ''
-            #!${pkgs.python3.interpreter}
-            import smtplib
-
-            with smtplib.SMTP('${domain}') as smtp:
-              smtp.ehlo()
-              smtp.login("alice","foobar")
-              smtp.quit()
-          '';
-
-          authStarttls = pkgs.writeScriptBin "authStarttls" ''
-            #!${pkgs.python3.interpreter}
-            import smtplib
-            import ssl
-
-            ctx = ssl.create_default_context()
-
-            with smtplib.SMTP('${domain}') as smtp:
-              smtp.ehlo()
-              smtp.starttls(context=ctx)
-              smtp.ehlo()
-              smtp.login("alice","foobar")
-              smtp.quit()
-          '';
-
-          authSmtps = pkgs.writeScriptBin "authSmtps" ''
-            #!${pkgs.python3.interpreter}
-            import smtplib
-            import ssl
-
-            ctx = ssl.create_default_context()
-
-            with smtplib.SMTP_SSL('${domain}', context=ctx) as smtp:
-              smtp.ehlo()
-              smtp.login("alice","foobar")
-              smtp.quit()
-          '';
         in
         [
           sendTestMail
